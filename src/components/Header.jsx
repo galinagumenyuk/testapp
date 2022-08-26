4e9c3206d2d47fe261117d4fa750a2b1b4393925
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-08-26 20:08:17 +0300

    fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import s from "./Header.module.css";
-import { ReactComponent as Svg } from '../images/svg/magnifier-search-zoom-svgrepo-com.svg';
-import {ReactComponent as SvgBurger} from '../images/svg/hamburger-menu-more-svgrepo-com.svg';
+import { ReactComponent as Svg } from '../images/svg/ux-user-app-svgrepo-com.svg';
+import {ReactComponent as SvgBurger} from '../images/svg/ux-basic-app-svgrepo-com.svg';
 
 const Header = () => { 
     return (
         <div className={s.container}>
             <button type="button" className={s.button}>
-                <Svg/>
+                <SvgBurger/>
             </button>
             <button type="button" className={s.button}>
-                <SvgBurger/>
+                <Svg />
             </button>
         </div> 
     )
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,7 +4,7 @@ import s from "./Quote.module.css";
 const Quote = () => { 
     return (
         <div className={s.container}>
-            <h2 className={s.quote}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam fugit quos labore consequatur suscipit!"</h2>
+            <h2 className={s.quote}>"Не роби мені нєрви, бо хапнеш:)"</h2>
             <p className={s.author}>Author</p>
         </div> 
     )
