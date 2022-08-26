3bc6c14c9a687df75babd616fba63a4e509034e0
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-08-26 22:17:42 +0300

    dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Header.module.css";
 import { ReactComponent as Svg } from '../images/svg/ux-user-app-svgrepo-com.svg';
-import {ReactComponent as SvgBurger} from '../images/svg/ux-basic-app-svgrepo-com.svg';
+import { ReactComponent as SvgBurger } from '../images/svg/ux-basic-app-svgrepo-com.svg';
+
+
+
 
 const Header = () => { 
+    const [clicked, setClicked] = useState(false);
+    
+
     return (
         <div className={s.container}>
-            <button type="button" className={s.button}>
+            <button type="button" className={s.button} onClick={() => { setClicked(!clicked) }}>
                 <SvgBurger/>
             </button>
+            {clicked && <div id="myDropdown" className={s.dropdown}>
+                <li className={s.item}>Мотиваційні</li>
+                <li className={s.item}>Нудні</li>
+                <li className={s.item}>Якась хєрня</li>
+            </div>}
             <button type="button" className={s.button}>
                 <Svg />
             </button>
